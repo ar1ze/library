23671909baf8065c365d476144f05d6a4efc7709
Eric Le <eriiicle@outlook.com>
2025-10-04 13:15:48 +0200

    feat(dialog): style add book form

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addBookBtn = document.querySelector('.add-book-btn');
 const addBookDialog = document.querySelector('.add-book-dialog');
+
 const dialogCloseBtn = document.querySelector('.add-book-dialog__close-btn');
+
 const dialogSubmitBtn = document.querySelector('.add-book-form__submit-btn');
 
 addBookBtn.addEventListener('click', () => {
