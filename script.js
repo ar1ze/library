68153a2fe46d8eee0096fd8e032e83896c90ec58
Eric Le <eriiicle@outlook.com>
2025-10-07 08:17:30 +0200

    feat(book): add book object to library from form

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
+const myLibrary = [];
+
 const addBookBtn = document.querySelector('.add-book-btn');
-const addBookDialog = document.querySelector('.add-book-dialog');
 
+const dialog = document.querySelector('.add-book-dialog');
 const dialogCloseBtn = document.querySelector('.add-book-dialog__close-btn');
+const dialogForm = document.querySelector('.add-book-dialog__form');
 
-const dialogSubmitBtn = document.querySelector('.add-book-form__submit-btn');
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.id = crypto.randomUUID();
+}
 
-addBookBtn.addEventListener('click', () => {
-  addBookDialog.showModal();
-});
+function showDialog() {
+  dialog.showModal();
+}
 
-dialogCloseBtn.addEventListener('click', () => {
-  addBookDialog.close();
-});
+function closeDialog() {
+  dialog.close();
+}
+
+function dialogSubmit(event) {
+  event.preventDefault();
+
+  const formData = new FormData(event.target);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const pages = formData.get('pages');
+  const read = formData.has('is-read');
+
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+
+  event.target.reset();
+  closeDialog();
+}
+
+addBookBtn.addEventListener('click', showDialog);
+
+dialogCloseBtn.addEventListener('click', closeDialog);
+dialogForm.addEventListener('submit', dialogSubmit);
